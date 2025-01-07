d99ac11f45469ece5d46ea19610034a10c801a11
Dong Hyeon Kim <freeman9998@naver.com>
2025-01-07 10:53:14 +0900

    webpack 설정

--- react-app/vite.config.ts
+++ react-app/vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
+      "@styles": path.resolve(__dirname, "./src/styles"),
+      "@api": path.resolve(__dirname, "./src/api"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@store": path.resolve(__dirname, "./src/store"),
+    },
+  },
 })
